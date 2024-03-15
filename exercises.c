e6ffa5c821f514da16f4ccd41c23277ca1792a07
Your Name <you@example.com>
2024-03-15 00:34:46 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -115,13 +115,13 @@ int checkSorted(int arr[], int size)
   bool ascendente = false;
   bool descendiente = false;
 
-  for (int i = 0; i < size - 1 ; i++)
+  for (int i = 1; i < size; i++)
     {
-      if (arr[i] < arr[i + 1])
+      if (arr[i] > arr[i-1])
       {
         ascendente = true;
       }
-      if (arr[i] > arr[i + 1])
+      if (arr[i] < arr[i-1])
       {
         descendiente = true;
       }  
